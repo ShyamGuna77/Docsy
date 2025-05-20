04f708a267d9db6d62d838dec2e0df7ab71541a7
Shyam <gshyamprasad2001@gmail.com>
2025-05-20 23:39:44 +0530

    auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://strong-ghoul-27.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
