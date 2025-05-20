d0e8f1b622621ebabce7f3aba78bf52cdf851ddb
Shyam <gshyamprasad2001@gmail.com>
2025-05-20 17:46:21 +0530

    featAdded Templates

---
+++ src/hooks/use-search-param.ts
@@ -0,0 +1,12 @@
+
+
+import { parseAsString, useQueryState } from "nuqs";
+
+export function useSearchParam() {
+  return useQueryState(
+    "search",
+    parseAsString.withDefault("").withOptions({ clearOnDefault: true }),
+  )
+};
+
+
