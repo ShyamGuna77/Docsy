5b2b07390585ab052780742995950b9cce3e93d6
Shyam <gshyamprasad2001@gmail.com>
2025-05-20 23:27:35 +0530

    feat:added tables and components

--- src/landing/page.tsx
+++ src/landing/page.tsx
@@ -1,11 +1,38 @@
 
+"use client";
 
-import React from 'react'
+import { usePaginatedQuery } from "convex/react";
+
+import { useSearchParam } from "@/hooks/use-search-param";
+
+import { Navbar } from "./navbar";
+import { DocumentsTable } from "./document-table";
+import { TemplatesGallery } from "./templates-gallery";
+import { api } from "../../convex/_generated/api";
 
 const Landing = () => {
-  return (
-    <div>Landing</div>
-  )
-}
+  const [search] = useSearchParam();
+  const { results, status, loadMore } = usePaginatedQuery(
+    api.documents.get,
+    { search },
+    { initialNumItems: 5 }
+  );
 
-export default Landing
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
+      </div>
+      <div className="mt-16">
+        <TemplatesGallery />
+        <DocumentsTable
+          documents={results}
+          loadMore={loadMore}
+          status={status}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
